be72a4abdcf974912d7aabd253619c7cb0df2a31
Shobhit Bhargava <shobhitbhargava@yahoo.com>
2024-07-04 12:43:55 +0530

    new commit

--- Spring.IO.ZeroToHero/src/main/java/com/spring/app/Alien.java
+++ Spring.IO.ZeroToHero/src/main/java/com/spring/app/Alien.java
@@ -1,12 +1,16 @@
 package com.spring.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
 
+	@Autowired
+	Laptop laptop;
+	
 	public void code() {
-		System.out.println("Coding");
+		laptop.compile();
 	}
 	
 }
